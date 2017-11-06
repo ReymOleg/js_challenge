86bc9ebfe3739105f97b35d126dc01e4631a3832
unknown <you@example.com>
2017-11-06 17:13:33 +0300

    t1+t2

--- task_1/sumOfTwo.js
+++ task_1/sumOfTwo.js
@@ -1,3 +1,23 @@
 function sumOfTwo(array1, array2, x) {
-
+  for(var i in array1) {
+    for(var j in array2) {
+      if(array1[i] + array2[j] === x) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
+
+console.warn('task 1');
+
+var array1 = [], array2 = [], x;
+array1 = [1,2,3,4,5];
+array2 = [100, 200, 300];
+x = 103;
+console.log(sumOfTwo(array1, array2, x));
+
+array1 = [1,2,3,4,5];
+array2 = [100, 200, 300];
+x = 107;
+console.log(sumOfTwo(array1, array2, x));
--- task_2/houseRobber.js
+++ task_2/houseRobber.js
@@ -1,3 +1,21 @@
 function houseRobber(nums) {
+  var maxKeyValue = {
+    key: nums.indexOf(Math.max.apply(Math,nums)),
+    value: Math.max.apply(Math,nums)
+  };
 
+  var maxSum = 0;
+  for(var k in nums) {
+    if(Math.abs(k - maxKeyValue.key) > 1) {
+      var currentSum = nums[k] + maxKeyValue.value;
+      maxSum = currentSum > maxSum ? currentSum : maxSum;
+    }
+  }
+
+  return maxSum;
 }
+
+var nums = [1, 3, 1, 10, 100, 4];
+
+console.warn('task 2');
+console.log(houseRobber(nums));
